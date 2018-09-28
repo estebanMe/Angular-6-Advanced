7755e023f958d96ceb02ee8b4f43f3a184568295
Esteban Mercado <v.estebanmercado@gmail.com>
2018-09-27 21:09:22 -0300

    config route module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { Routes } from "@angular/router";
+
+import { RouterModule, Routes } from "@angular/router";
+
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
 
 
 const appRoutes: Routes = [
@@ -15,3 +17,5 @@ const appRoutes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: '**', component: NopagefoundComponent },
 ];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
